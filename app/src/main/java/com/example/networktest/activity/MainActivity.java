7f8477d19681b6ab5fdb8b953c5426beb032ebe3
yuanbao97 <316261393@qq.com>
2021-01-27 14:20:40 +0800

    init

---
+++ app/src/main/java/com/example/networktest/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.networktest.activity;
+
+import android.os.Bundle;
+
+import com.example.networktest.R;
+import com.example.networktest.framework.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
